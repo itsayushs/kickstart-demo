fd10977cfb9e534df5afd6996c609db6939e8e92
root <root@ip-172-31-42-120.ap-south-1.compute.internal>
2020-02-02 16:41:18 +0000

    added new code

--- code.py
+++ code.py
@@ -1,3 +1,12 @@
-for i in range(0,10):
-	print('Hey World!')
+name = input("Name: ")
+time = input("Time Am/Pm: ")
+if time == 'am':
+    print('good morning! '+name)
+else:
+    print('good night! '+name)
 
+a = 10 
+b = 20 
+c = 30 
+result = a+b+c
+print('Important Cal: '+ str(result))
